3d95a5d14f64749faea7c631432dc8756675aae3
shipclone06 <anhkhoa030900@gmail.com>
2025-03-10 08:51:33 +0700

    1231

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,12 +37,16 @@
             this.btnlogin = new System.Windows.Forms.Button();
             this.btnexit = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblinfo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
             this.SuspendLayout();
             // 
             // txtpass
             // 
-            this.txtpass.Location = new System.Drawing.Point(80, 60);
+            this.txtpass.Location = new System.Drawing.Point(80, 80);
             this.txtpass.Name = "txtpass";
             this.txtpass.PasswordChar = '*';
             this.txtpass.Size = new System.Drawing.Size(158, 20);
@@ -50,7 +54,7 @@
             // 
             // txtuser
             // 
-            this.txtuser.Location = new System.Drawing.Point(80, 17);
+            this.txtuser.Location = new System.Drawing.Point(80, 40);
             this.txtuser.Name = "txtuser";
             this.txtuser.Size = new System.Drawing.Size(158, 20);
             this.txtuser.TabIndex = 1;
@@ -58,7 +62,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Location = new System.Drawing.Point(12, 83);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 9;
@@ -67,7 +71,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(12, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 8;
@@ -75,16 +79,17 @@
             // 
             // btnlogin
             // 
-            this.btnlogin.Location = new System.Drawing.Point(51, 108);
+            this.btnlogin.Location = new System.Drawing.Point(47, 123);
             this.btnlogin.Name = "btnlogin";
             this.btnlogin.Size = new System.Drawing.Size(75, 36);
             this.btnlogin.TabIndex = 3;
             this.btnlogin.Text = "Đăng nhập";
             this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
             // 
             // btnexit
             // 
-            this.btnexit.Location = new System.Drawing.Point(163, 108);
+            this.btnexit.Location = new System.Drawing.Point(163, 123);
             this.btnexit.Name = "btnexit";
             this.btnexit.Size = new System.Drawing.Size(75, 36);
             this.btnexit.TabIndex = 4;
@@ -96,12 +101,38 @@
             // 
             this.errorProvider1.ContainerControl = this;
             // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Bảo Mật";
+            // 
+            // lblinfo
+            // 
+            this.lblinfo.AutoSize = true;
+            this.lblinfo.BackColor = System.Drawing.Color.White;
+            this.lblinfo.ForeColor = System.Drawing.Color.Red;
+            this.lblinfo.Location = new System.Drawing.Point(44, 173);
+            this.lblinfo.Name = "lblinfo";
+            this.lblinfo.Size = new System.Drawing.Size(0, 13);
+            this.lblinfo.TabIndex = 11;
+            this.lblinfo.Click += new System.EventHandler(this.lblinfo_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(294, 184);
+            this.ClientSize = new System.Drawing.Size(294, 195);
+            this.Controls.Add(this.lblinfo);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtpass);
             this.Controls.Add(this.txtuser);
             this.Controls.Add(this.label2);
@@ -112,6 +143,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = resources.GetString("$this.Text");
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,6 +158,9 @@
         private System.Windows.Forms.Button btnlogin;
         private System.Windows.Forms.Button btnexit;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.Label lblinfo;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,43 @@ namespace dethicuoiki
         {
             this.Close();
         }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string u = "shipclone06";
+            string p = "0x98_60";
+            if (txtuser.Text == u && txtpass.Text == p)
+            {
+                Form2 form2 = new Form2();
+                this.Hide();
+                form2.ShowDialog();
+                this.Close();
+            }
+            
+
+            if (txtuser.TextLength == 0)
+            {
+                errorProvider1.SetError(txtuser, "vui lòng ghi thông tin");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+            if (txtpass.TextLength == 0)
+            {
+                errorProvider2.SetError(txtpass, "vui lòng ghi thông tin");
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+            lblinfo.Text = "Sai rồi kìa, vui lòng nhập lại tên hoặc mật khẩu";
+
+        }
+
+        private void lblinfo_Click(object sender, EventArgs e)
+        {
+            lblinfo.Text = "";
+        }
     }
 }
